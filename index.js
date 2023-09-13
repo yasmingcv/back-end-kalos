b6d4fd17e44d28b93244da36a24455f31a7c85e9
YASMIN GONÇALVES COELHO <22282215@dsn127m1242267.cfp127aluno.local>
2023-09-13 15:43:34 -0300

    vai 2.0

--- index.js
+++ index.js
@@ -245,18 +245,18 @@ app.delete('/kalos/aluno/id/:id', cors(), async function(request, response){
 app.post('/kalos/aluno/autenticar', cors(), bodyParserJSON, async function(request, response){
     let contentType = request.headers['content-type']
     
-    if(String(contentType).toLowerCase() == 'application/json'){
+    // if(String(contentType).toLowerCase() == 'application/json'){
         let dadosBody = request.body
         let resultDadosAluno = await controllerAluno.autenticarAluno(dadosBody)
 
         response.status(200)
         response.json(resultDadosAluno)
 
-    } else {
-        response.status(message.ERROR_INVALID_CONTENT_TYPE.status)
-        response.json(message.ERROR_INVALID_CONTENT_TYPE)
+    // } else {
+    //     response.status(message.ERROR_INVALID_CONTENT_TYPE.status)
+    //     response.json(message.ERROR_INVALID_CONTENT_TYPE)
 
-    }
+    // }
 
 })
 
